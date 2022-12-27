65d797ac6e2facdd6efc33ade2b3b208dff9a90b
malgin1983 <malghin@yandex.ru>
2022-12-28 01:48:01 +0300

    Added main structure and methods

---
+++ linkedList.js
@@ -0,0 +1,86 @@
+const createNode = (data) => ({
+    data,
+    next: null
+})
+
+const LinkedList =() => ({
+    head: null,
+    tail: null,
+    toArray() {
+        const arr = [];
+        let currentNode = this.head
+        while (currentNode){
+            arr.push(currentNode.data)
+            currentNode = currentNode.next
+        }
+        return arr;
+    },
+    addFirst(d) {
+        if(!d) return this
+        const node = createNode(d)
+        const newNode = node
+        newNode.next = this.head
+        this.head = newNode
+        if( !this.tail ){
+            this.tail = newNode;
+        }
+        return this;
+    },
+    addLast(d) {
+        if(!d) return this
+        const node = createNode(d)
+        if( !this.head || !this.tail ){
+            this.head = node;
+            this.tail = node;
+        }
+        this.tail.next = node;
+        this.tail = node;
+        return this;
+    },
+    removeFirst() {
+        if(this.head){
+            this.head = this.head.next
+            return this
+        } else {
+            throw new Error("List don't have elements")
+        }
+    },
+    removeLast() {
+        if(this.tail){
+            let prevNode = this.head;
+            let currentNode = this.head
+            while (currentNode.next) {
+                prevNode = currentNode
+                currentNode = currentNode.next
+            }
+            this.tail = prevNode;
+            return this
+        } else {
+            throw new Error("List don't have elements")
+        }
+    },
+    getFirst() {
+        if (!this.head) return null
+        return this.head.data;
+    },
+    getLast() {
+        if (!this.tail) return null
+        return this.tail.data;
+    },
+    getLength() {
+        if(!this.head) return 0
+        const arr = [];
+        let currentNode = this.head
+        while (currentNode){
+            arr.push(currentNode.data)
+            currentNode = currentNode.next
+        }
+        return arr?.length;
+    }
+})
+
+module.exports = {
+    LinkedList : LinkedList,
+    createNode
+}
+
---
+++ linkedList.test.js
@@ -0,0 +1,96 @@
+const { LinkedList } = require('./linkedList')
+
+
+describe('Test add function LinkedList', () => {
+    const node_1 = 'one';
+    const node_2 = 'two';
+    const node_3 = 'three';
+    const node_4 = 'four';
+    const node_5 = 'five';
+    
+    test('Empty node add first', () => {
+        expect(LinkedList().addFirst().toArray().length).toEqual(0);
+    });
+    
+    test('Empty node add last', () => {
+        expect(LinkedList().addLast().toArray().length).toEqual(0);
+    });
+    
+    test('Added two elements addFirst', () => {
+        const resArr = LinkedList().addFirst(node_2).addFirst(node_1).toArray()
+        expect(resArr.length).toEqual(2);
+        expect(resArr[0]).toEqual('one');
+        expect(resArr[1]).toEqual('two');
+    });
+    
+    test('Added two elements addLast', () => {
+        const listArr = LinkedList().addFirst(node_3).addFirst(node_4).addFirst(node_5).toArray()
+        expect(listArr.length).toEqual(3);
+        expect(listArr[0]).toEqual('five');
+        expect(listArr[1]).toEqual('four');
+        expect(listArr[2]).toEqual('three');
+    });
+    
+    const list2 = LinkedList()
+    list2.addFirst(node_3)
+    list2.addLast()
+    const node_6 = 6;
+    const node_7 = 7;
+    test('Get first and last  element', () => {
+        expect(list2.getFirst()).toEqual('three');
+        expect(list2.getLast()).toEqual('three');
+        list2.addFirst(node_6)
+        expect(list2.getFirst()).toEqual(6);
+        list2.addLast(node_7)
+        expect(list2.getLast()).toEqual(7);
+    });
+});
+
+describe('Test remove function LinkedList', () => {
+    const one = 'one';
+    const two = 'two';
+    const three = 'three';
+    const four = 'four';
+    const five = 'five';
+    
+    const list11 = LinkedList().addFirst(two).addFirst(one).addLast(three).addLast(four).addLast(five)
+
+    test('Empty List removeFirst', () => {
+        try {
+            expect(LinkedList().removeFirst())
+        } catch(e) {
+            expect(e.message).toBe("List don't have elements")
+        }
+    });
+    
+    test('Empty List removeLast', () => {
+        try {
+            expect(LinkedList().removeLast())
+        } catch(e) {
+            expect(e.message).toBe("List don't have elements")
+        }
+    });
+    
+    test('List removeFirst', () => {
+        list11.removeFirst()
+        expect(list11.getFirst()).toEqual('two');
+    });
+    
+    test('List removeLast', () => {
+        list11.removeLast()
+        expect(list11.getLast()).toEqual('four');
+    });
+});
+
+describe('Test length', () => {
+    const list3 = LinkedList()
+    const list4 = LinkedList().addFirst('a').addLast('b').addLast('c')
+    
+    test('Empty getLength', () => {
+        list3.getLength()
+    });
+    
+    test('List getLength', () => {
+        expect(list4.getLength()).toEqual(3);
+    });
+});
